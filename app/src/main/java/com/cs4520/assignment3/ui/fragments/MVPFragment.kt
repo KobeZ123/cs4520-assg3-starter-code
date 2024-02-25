8b93758ca6e991a7e670aa70e06662c9ca2afba1
Kobe <kobezhe@gmail.com>
2024-02-25 12:35:56 -0500

    fragment xml files + view binding

--- app/src/main/java/com/cs4520/assignment3/ui/fragments/MVPFragment.kt
+++ app/src/main/java/com/cs4520/assignment3/ui/fragments/MVPFragment.kt
@@ -5,15 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.cs4520.assignment3.R
+import com.cs4520.assignment3.databinding.FragmentMvpBinding
 
 class MVPFragment : Fragment() {
+    private var _binding: FragmentMvpBinding? = null
+    private val binding get() = _binding!!
 
+    /**
+     * creates the view with view binding
+     */
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_mvp_fragment, container, false)
+    ): View {
+        super.onCreate(savedInstanceState)
+        _binding = FragmentMvpBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/com/cs4520/assignment3/ui/fragments/MVVMFragment.kt
+++ app/src/main/java/com/cs4520/assignment3/ui/fragments/MVVMFragment.kt
@@ -5,15 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.cs4520.assignment3.R
+import com.cs4520.assignment3.databinding.FragmentMvvmBinding
 
 class MVVMFragment: Fragment() {
+    private var _binding: FragmentMvvmBinding? = null
+    private val binding get() = _binding!!
 
+    /**
+     * creates the view with view binding
+     */
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_mvvm_fragment, container, false)
+    ): View {
+        super.onCreate(savedInstanceState)
+        _binding = FragmentMvvmBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
